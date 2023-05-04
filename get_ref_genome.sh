ea02e9beb1b1fbc1fbe4be13dceda1d95261f174
qverron <97879071+qverron@users.noreply.github.com>
2023-05-04 11:25:48 +0200

    Update get_ref_genome.sh

--- get_ref_genome.sh
+++ get_ref_genome.sh
@@ -4,7 +4,16 @@
 prefix="Mus_musculus.GRCm38.dna"
 address="http://ftp.ensembl.org/pub/release-102/fasta/mus_musculus/dna/"
 total=19
-# ex: get-ref-genome.sh 
+
+# Human GRCh37:
+#prefix="Homo_sapiens.GRCh37.dna"
+#address="https://ftp.ensembl.org/pub/grch37/release-109/fasta/homo_sapiens/dna/"
+#total=22
+
+# Human GRCh38:
+#prefix="Homo_sapiens.GRCh38.dna"
+#address="https://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/“
+#total=22
 
 mkdir -p data/ref
 cd data
