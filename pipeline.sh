d263203a70dc097ffbad07c3a9d3df6e574c11e3
qverron <97879071+qverron@users.noreply.github.com>
2023-06-19 11:20:53 +0200

    Add files via upload

--- pipeline.sh
+++ pipeline.sh
@@ -16,31 +16,33 @@ mkdir HUSH
 # 1. Reformat input
 Rscript prepare_input.r
 
-# 2. Retrieve sequences and extract k-mers
+# 2. Generate black list of abundantly repeated oligos for each reference genome
+./generate_blacklist.sh -L 40 -c 100
+# only needs to be run once for a given reference genome
+
+# 3. Retrieve sequences and extract k-mers
 ./get_oligos.py DNA/RNA [applyGCfilter 0/1] [extfolder] # RNA assumes already existing transcript sequences. Default> DNA
 
-# 3. Run nHUSH and reconstitute into full-length oligos if using sublength hashing
+# 4. Run nHUSH and reconstitute into full-length oligos if using sublength hashing
 ./run_nHUSH.sh -d DNA -L 40 -l 21 -m 1 -t 40 -i 14 # 40 threads for max perf. Add -g for genome ref
 ./run_nHUSH.sh -d RNA -L 35 -m 5 -t 40 -i 14
 
 # only in case HUSH didn't finish successfully
 ./unfinished_HUSH.sh
 
-# only if using sublength:
-./reform_hush_consec.py DNA 40 21  # syntax: ./reform_hush.py DNA|RNA|-RNA length (sublength)
-./reform_hush.py DNA|RNA|-RNA length (sublength)
+# 5. Recapitulate HUSH results
 ./reform_hush_combined.py DNA|RNA|-RNA length sublength until
 
-# 4. Calculate secondary structures and melting temperature
+# 6. Calculate secondary structures and melting temperature
 ./melt_secs_parallel.sh DNA|RNA
 
-# 5. Create database and convert to TSV for querying. Attribute score to each oligo
+# 7. Create database and convert to TSV for querying. Attribute score to each oligo
 ./build-db.sh q_combined 32 6 70 #(optional score function: q/q_combined, default: q)
-./build-db_cc.sh q_cc 32 6  #score function: q_cc
+./build-db_BL.sh -f q_bl -m 32 -i 6 -L 40 -c 100 -d 8 -T 72 #score function: q_cc
 # 32: length of max consecutive perfect match allowed; 6: max number of consecutive identical base pairs, 70: target temperature
 
-# 6. Query to fetch and optimize probes
-./cycling_query.py -s RNA -L 30 -m 7 -c 50 -t 40    
+# 8. Query to fetch and optimize probes
+./cycling_query.py -s RNA -L 30 -m 7 -c 50 -t 40 -greedy  
 # [recommended: -greedy     for speed > quality. Can be removed when designing final oligos]        
 # [optional: -start 20 -end 100 -step 5: to sweep different oligo numbers]
 # -stepdown 10       how many oligos to decrease with every iteration 
