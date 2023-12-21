5971dbc8e451eaeef872cd6dcd48f1e047fbfc5a
Zaf4 <zafermolbio@gmail.com>
2023-12-21 14:34:53 +0100

    remove execution compatibility

--- pipeline_exclude.sh
+++ pipeline_exclude.sh
@@ -1,6 +1,6 @@
 # run all the steps of the pipeline
 # 0. if necessary, retrieve the reference genome
-./get_ref_genome.sh           # /!\ the settings need to be adjusted manually!!
+bash ./get_ref_genome.sh           # /!\ the settings need to be adjusted manually!!
 
 # 0. Create all the directories for output
 mkdir data/candidates
@@ -10,38 +10,37 @@ mkdir data/db
 mkdir data/db_tsv
 mkdir data/probe_candidates
 mkdir data/selected_probes
-
 mkdir HUSH
 
 # 1. Reformat input
 Rscript prepare_input.r
 
 # 2. Retrieve sequences and extract k-mers
-./get_oligos.py DNA/RNA [applyGCfilter False] [extfolder] # RNA assumes already existing transcript sequences. Default> DNA
+python ./get_oligos.py DNA/RNA [applyGCfilter False] [extfolder] # RNA assumes already existing transcript sequences. Default> DNA
 
 # 3. The exclusion pipeline requires a mask of regions to exclude from HUSH analysis. 
 # These can either be added manually in data/exclude or automatically generated to only include the ROI itself. 
 # Minimum: (use as template if adding more masks)
-./generate_exclude.py
+python ./generate_exclude.py
 
 # 4. Generate masked reference genomes
-./exclude_region.py 
+python ./exclude_region.py 
 
 # 5. Generate black list of abundantly repeated oligos for each reference genome
-./generate_blacklist.sh -L 40 -c 100
+bash ./generate_blacklist.sh -L 40 -c 100
 # L: oligo length; c: min abundance to be included in oligo black list
 
 # 6. Run nHUSH and reconstitute into full-length oligos if using sublength hashing
-./run_nHUSH_excl.sh -d DNA -L 40 -l 21 -m 3 -t 40 -i 14 # 40 threads for max perf.
+bash ./run_nHUSH_excl.sh -d DNA -L 40 -l 21 -m 3 -t 40 -i 14 # 40 threads for max perf.
 
 # only in case HUSH didn't finish successfully
-./unfinished_HUSH.sh
+bash ./unfinished_HUSH.sh
 
 # 7. only if using sublength:
-./reform_hush_combined.py DNA|RNA|-RNA length sublength until
+python ./reform_hush_combined.py DNA|RNA|-RNA length sublength until
 
 # 8. Calculate secondary structures and melting temperature
-./melt_secs_parallel.sh DNA|RNA
+bash ./melt_secs_parallel.sh DNA|RNA
 
 # 9. Create database and convert to TSV for querying. Attribute score to each oligo
 ./build-db.sh q_combined 32 6 70 #(optional score function: q/q_combined, default: q)
@@ -52,7 +51,7 @@ Rscript prepare_input.r
 # i: max identical consecutive base pairs, T: target temperature, m: max length of consecutive off-target match
 
 # 10. Query to fetch and optimize probes
-./cycling_query.py -s DNA -L 40 -m 8 -c 100 -t 40 -g 500 -stepdown 50 -greedy -excl
+python ./cycling_query.py -s DNA -L 40 -m 8 -c 100 -t 40 -g 500 -stepdown 50 -greedy -excl
 # [recommended: -greedy     for speed > quality. Can be removed when designing final oligos]        
 # [optional: -start 20 -end 100 -step 5: to sweep different oligo numbers]
 # -stepdown 10       how many oligos to decrease with every iteration 
@@ -63,5 +62,3 @@ python summarize-probes-final.py
 # TO DO: ADD PLOTS
 plot_oligos.ipynb
 plot_probe_candidates.ipynb
-
-
