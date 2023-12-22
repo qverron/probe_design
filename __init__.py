21c3123ea545c90249fa3b6d45e98edf40979793
Zaf4 <zafermolbio@gmail.com>
2023-12-22 15:08:57 +0100

    form function into a module

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# Expose this functions to outside as a module
+from src import (
+    cycling_query,
+    escafish_score,
+    exclude_region,
+    generate_exclude,
+    get_oligos,
+    HUSH_feedback,
+    reform_hush_combined,
+    reform_hush_consec,
+    reform_hush,
+    select_probe,
+    split_fasta,
+    summarize_probes_cumul,
+    summarize_probes_final,
+    summarize_probes)
