716e494647019fb83761ad425f427cd9c5806326
Zaf4 <zafermolbio@gmail.com>
2024-01-30 10:01:04 +0100

    add colored succes message

--- probe_design/src/test.py
+++ probe_design/src/test.py
@@ -3,17 +3,25 @@ import sys
 
 # add argparser that takes -l and -m and multiply them
 argparser = argparse.ArgumentParser()
-argparser.add_argument("-l", type=int, help="length")
-argparser.add_argument("-x", type=int, help="height")
-argparser.add_argument("-w", type=int, help="width")
-argparser.add_argument("-s", type=str, help="shape")
+argparser.add_argument("-l", type=int, help="length", default=1)
+argparser.add_argument("-x", type=int, help="height", default=2)
+argparser.add_argument("-w", type=int, help="width", default=3)
+argparser.add_argument("-s", type=str, help="shape", default="box")
 args = argparser.parse_args()
 
+def main():
 
-if __name__ == "__main__":
+    green_code = "\033[92m" # Green
+    reset_code = "\033[0m"  # Reset color
+    red_code = "\033[91m"   # Red
 
     volume = args.l * args.x * args.w
     shape = args.s
+    print(f"{red_code}Length: {args.l}, Height: {args.x}, Width: {args.w}{reset_code}")
     print(f"Volume of {shape} is {volume}")
 
-    # prb test -l 10 -h 20 -w 30 -s box
+    print(f"{green_code}TEST SUCCESFUL!{reset_code}")
+    # prb test -l 10 -x 20 -w 30 -s box
+if __name__ == "__main__":
+
+    main()
