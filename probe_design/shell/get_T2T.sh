22a1d23b8cc865a92710cb5ea9c6395c3d4f6ff9
Zaf4 <zafermolbio@gmail.com>
2024-05-14 12:46:28 +0200

    fix get_T2T

--- probe_design/shell/get_T2T.sh
+++ probe_design/shell/get_T2T.sh
@@ -4,5 +4,5 @@ mkdir -p data/ref
 cd data/ref
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/914/755/GCA_009914755.4_T2T-CHM13v2.0/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz
 gunzip GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz
-prb split_T2T.py -g GCA_009914755.4_T2T-CHM13v2.0_genomic.fna -p CHM13.T2T -s .
+prb split_T2T -g GCA_009914755.4_T2T-CHM13v2.0_genomic.fna -p CHM13.T2T -s .
 mv GCA_009914755.4_T2T-CHM13v2.0_genomic.fna genome.fa
